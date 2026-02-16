9cf8c8c88b414a91f52de4ed6c4b2fdd5f4d59e5
CioaraRaul <cioararaul08@gmail.com>
2026-02-16 18:01:09 +0200

    fixed bug with login with google

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -9,10 +9,11 @@ export default [
     route("callback", "routes/auth/callback.tsx"),
     route("privacy-policy", "routes/auth/privacyPolicy.tsx"),
     route("terms", "routes/auth/terms.tsx"),
-    route("auth/verify-email", "routes/auth/verifyEmail.tsx"),
+    route("verify-email", "routes/auth/verifyEmail.tsx"),
   ]),
 
   route("", "routes/app-layout.tsx", [
+    index("routes/home.tsx"),
     route("", "routes/homepage/index.tsx"),
     route("dashboard", "routes/homepage/main/dashboard.tsx", []),
     route("dashboard/messages", "routes/homepage/main/messages.tsx"),
